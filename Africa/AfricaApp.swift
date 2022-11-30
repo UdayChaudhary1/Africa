63af614425d59a4b341c2aa0e81ea5997f2cef92
UdayChaudhary1 <chaudharyuday081@gmail.com>
2022-11-30 19:25:35 +0530

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Uday Chaudhary on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
